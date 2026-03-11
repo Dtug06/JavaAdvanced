e4e4ca12d4d2b3a5a2f32deac538898bad545524
Dtug06 <tuan20062012@gmail.com>
2026-03-11 22:00:34 +0700

    ss4

---
+++ SS3/bai5/bai5Test.java
@@ -0,0 +1,7 @@
+package SS3.bai5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class bai5Test {
+
+}
---
+++ SS4/bai2/UserService.java
@@ -0,0 +1,9 @@
+package SS4.bai2;
+
+public class UserService {
+    public boolean checkRegistrationAge(int age) throws IllegalAccessException {
+        if (age < 0) throw new IllegalAccessException("Tuoi khong hop le");
+        return age >= 18;
+    }
+
+}
